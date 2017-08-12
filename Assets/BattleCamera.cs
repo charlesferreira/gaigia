5644a3daba532a05eab76b8b44c6e9930c28e15c
Charles Ferreira <charles.dev@live.com>
2017-08-12 01:12:41 -0300

    Cria função de "pan" da câmera de batalha.

--- Assets/BattleCamera.cs
+++ Assets/BattleCamera.cs
@@ -4,25 +4,32 @@
 public class BattleCamera : MonoBehaviour {
 
     [SerializeField] private Transform target;
-    [SerializeField] private Vector3 offset;
+    [Range(0, 10)]
+    [SerializeField]
+    private float panDistance;
     [Range(0, 1)]
-    [SerializeField] private float damping;
+    [SerializeField]
+    private float damping;
 
-    new Camera camera;
+    Vector3 panOffset;
 
     public void SetUp(Transform target) {
         this.target = target;
     }
 
-    private void Awake() {
-        camera = GetComponent<Camera>();
-    }
-
     private void FixedUpdate() {
         if (target == null) return;
 
-        var distance = target.position + offset - transform.position;
+        UpdateOffset();
+        UpdatePosition();
+    }
 
+    private void UpdateOffset() {
+        panOffset.Set(Input.GetAxis("RightHorizontal"), 0, Input.GetAxis("RightVertical"));
+    }
+
+    private void UpdatePosition() {
+        var distance = target.position + (panOffset * panDistance) - transform.position;
         transform.position += distance * (1f - damping);
     }
 }

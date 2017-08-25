e510b03d8207a722907adc158ec5b31fb1e8d011
Charles Ferreira <charles.dev@live.com>
2017-08-25 11:29:25 -0300

    Cria um esquema simples de coleta de loot.

---
+++ Assets/Loot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Loot : MonoBehaviour {
+    
+    [SerializeField] private SpriteRenderer sprite;
+
+    private bool Collected { get; set; }
+
+    public void OnApproach() {
+        if (Collected) return;
+
+        sprite.color = Color.yellow;
+    }
+
+    public void OnLeave() {
+        sprite.color = Color.white;
+    }
+
+    public void Collect() {
+        Collected = true;
+        OnLeave();
+    }
+}
---
+++ Assets/LootCollector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LootCollector : MonoBehaviour {
+
+    private IList<Loot> LootNearby { get; set; }
+
+    private void OnTriggerEnter(Collider other) {
+        var loot = other.GetComponent<Loot>();
+        if (!loot) return;
+
+        loot.OnApproach();
+        LootNearby.Add(loot);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        var loot = other.GetComponent<Loot>();
+        if (!loot) return;
+
+        loot.OnLeave();
+        LootNearby.Remove(loot);
+    }
+
+    private void Awake() {
+        LootNearby = new List<Loot>();
+    }
+
+    private void Update() {
+        if (PlayerInput.Confirm && LootNearby.Count > 0) {
+            LootNearby[0].Collect();
+            LootNearby.RemoveAt(0);
+        }
+    }
+}

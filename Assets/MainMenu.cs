031727fa535a0cbd0e3686ae302036ae24988b34
Charles Ferreira <charles.dev@live.com>
2017-09-25 11:13:44 -0300

    Alterações menores no menu principal.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MainMenu : SimpleStateMachine<MainMenu> {
+
+    public Character Character { get; private set; }
 
     protected override IList<ISimpleState<MainMenu>> CreateStates() {
         return new List<ISimpleState<MainMenu>> {
             new Level1MainMenuState()
         };
+    }
+
+    new void Awake() {
+        base.Awake();
+        Character = CombatInfo.Instance.PlayerCharacters[0];
+        print(Character.Team);
     }
 }
